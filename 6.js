e535b41a9d15ce22fb889dd43450f20b15702752
sidalhussaini <syed.alhussaini@mail.mcgill.ca>
2021-11-15 23:55:39 -0500

    javascript test

--- 6.js
+++ 6.js
@@ -1,14 +1,10 @@
 function queryArtist () {
     let params = (new.URL(document.location)).searchParams;
     if (params.has('artist')){
-        let artistName = params.get('artist');
-        console.log(artistName);
-
+        let artistName = params.get('artist'); console.log(artistName);
         let mbBaseURL = "https://musicbrainz.org/ws/2/";
         let mbResource = "artist?query=";
-        let queryURL = mbBaseURL + mbResource + artistName;
-        console.log(queryURL);
-
+        let queryURL = mbBaseURL + mbResource + artistName; console.log(queryURL);
         httpGet(queryURL, getMBID);
     }
 }
@@ -31,14 +27,33 @@ function httpGet(theURL, cbFunction) {
     }
 }
 function getMBID(xhttp) {
-    let retrievedData = xhttp.responseXML;
-    console.log(retrievedData);
-    let artistData = retrievedData.getElementsByTagName("artist")[0];
-    console.log(artistData);
-    let artistName = artistData.getElementsByTagName("artist")[0].innerHTML;
-    console.log(artistName);
-    let artistMBID = artistData.id;
-    console.log(artistMBID);
-
+    let retrievedData = xhttp.responseXML; console.log(retrievedData);
+    let artistData = retrievedData.getElementsByTagName("artist")[0]; console.log(artistData);
+    let artistName = artistData.getElementsByTagName("artist")[0].innerHTML; console.log(artistName);
+    let artistMBID = artistData.id; console.log(artistMBID);
     queryAlbums(artistMBID);
 }
+function getAlbum(xhttp) {
+    let data = xhttp.responseXML; console.log(data);
+    let releaselist = data.getElementsByTagName("release-group-list")[0]; console.log(releaselist);
+    let releaseGroups = releaselist.getElementsByTagName("release-group");
+    let releaseCount = releaseGroups.length;
+    document.getElementById("albums").innerHTML = "There's" + releaseCount + "albums released."; console.log(releaseCount);
+    var AlbumNames = []; var AlbumDates = [];
+
+    for (let index = 0; index < releaseCount; index++) {
+        let albumData = releaselist.getElementsByTagName("release-group")[index];
+        let albumName = albumData.getElementsByTagName('title')[0].innerHTML; console.log(albumName);
+        AlbumNames[index] = albumName;
+        let albumDate = albumData.getElementsByTagName('first-release-date')[0].innerHTML; console.log(albumDate);
+        AlbumDates[index] = albumDate;
+    }
+    console.log(AlbumNames); console.log(AlbumDates);
+    text = "<tr><th>Album Name</th><th>Release Date</th></tr>";
+    for (i = 0; i < AlbumNames.length; i++) {
+        text += "<tr><td> " + AlbumNames[i] + "</td";
+        text += "td> " + AlbumDates [i] + "</td></tr>";
+    }
+    let placeholder = document.getElementById('placeholder'); placeholder.innerHTML = text;
+}
+window.onload = queryArtist;
